855a145822a11964f7dd5c686d15c27cf43e66d7
Michael Kutz <michael.kutz@rewe-group.com>
2026-02-11 17:48:56 +0100

    Refactor ProductController to use ports
    Replace direct repository access with port interfaces. Add ProductRequest
    and ProductResponse DTOs. Delete old Product entity, controller, and
    repositories.

--- src/main/kotlin/io/github/mkutz/code_review_workshop/product/ProductRepository.kt
+++
@@ -1,6 +0,0 @@
-package io.github.mkutz.code_review_workshop.product
-
-import java.util.UUID
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProductRepository : JpaRepository<Product, UUID>
